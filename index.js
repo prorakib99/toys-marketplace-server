3813c01989904a68ead7e40c2dac449cf514264f
Md Rakibul Islam <bdsky25@gmail.com>
2024-01-20 04:44:26 +0600

    Implement Single Toy load by specific Toy ID

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/my-toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.findOne(query);
+            res.send(result);
+        });
+
         app.delete('/my-toys/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
